1174320b725ed4a2f7d71c5ab481f5925d0a6c63
Daniel King <daniel.zidan.king@gmail.com>
2014-12-18 23:34:42 -0500

    Implement a few operators
     - OTypeof
     - OSurfaceTypeof
     - OPrimToNum
     - OPrimToStr
     - OPrimToBool
     - OIsPrim
     - OHasOwnProp

--- src/Lang/JS/Delta.hs
+++ src/Lang/JS/Delta.hs
@@ -1,6 +1,6 @@
 module Lang.JS.Delta where
 
-import Prelude (truncate, fromIntegral)
+import Prelude (truncate, fromIntegral, isNaN)
 import FP hiding (inject)
 
 import Data.Bits
@@ -88,8 +88,15 @@ evalOp o = case o of
   OLShift   -> binOp "LeftShift"          $ liftBinaryOpBot P P (shiftLeft          :: Double -> Double -> Double)
   OSpRShift -> binOp "SignedRightShift"   $ liftBinaryOpBot P P (signedShiftRight   :: Double -> Double -> Double)
   OZfRShift -> binOp "UnsignedRightShift" $ liftBinaryOpBot P P (unsignedShiftRight :: Double -> Double -> Double)
-  OStrictEq   -> binOp "TripleEquals" $ tripleEquals
-  OAbstractEq -> binOp "DoubleEquals" $ doubleEquals
+  OStrictEq   -> binOp   "TripleEquals" $ tripleEquals
+  OAbstractEq -> binOp   "DoubleEquals" $ doubleEquals
+  OTypeof     -> unaryOp "TypeOf"       $ typeof
+  OSurfaceTypeof -> undefined -- TODO: what is this?
+  OPrimToNum  -> unaryOp "PrimToNum"  $ primToNumber
+  OPrimToStr  -> unaryOp "PrimToStr"  $ primToString
+  OPrimToBool -> unaryOp "PrimToBool" $ primToBool
+  OIsPrim     -> unaryOp "IsPrim"     $ isPrim
+  OHasOwnProp -> binOp   "HasOwnProp" $ hasOwnProp
   where
     bAnd = fromInteger .: ((.&.) `on` Prelude.truncate)
     bOr  = fromInteger .: ((.|.) `on` Prelude.truncate)
@@ -132,3 +139,66 @@ evalOp o = case o of
     haskellInfinity = (1/0 :: Double)
     haskellNaN      = (0/0 :: Double)
     booleanToNumber b = if b then 1 else 0
+    typeof v = singleton $ LitA $ S $ case v of
+      -- TODO: 11.4.3 says soemthing about GetBase(v) = null do something special, what is that about?
+      (LitA NullL     ) -> "object"
+      (LitA UndefinedL) -> "undefined"
+      (LitA (B _)     ) -> "boolean"
+      (LitA (N _)     ) -> "number"
+      (LitA (S _)     ) -> "string"
+      NumA              -> "number"
+      StrA              -> "string"
+      BoolA             -> "boolean"
+      (CloA _)          -> "function"
+      (ObjA _)          -> "object"
+      (LocA _)          -> undefined -- This isn't part of real JS, should it be here?
+    primToNumber v = case v of
+      (LitA NullL     ) -> singleton $ pinject (0::Double)
+      (LitA UndefinedL) -> singleton $ pinject haskellNaN
+      (LitA (B b)     ) -> singleton $ pinject $ if b then (1::Double) else (0::Double)
+      (LitA (N n)     ) -> singleton $ pinject n
+      (LitA (S s)     ) -> singleton $ pinject (fromString' s :: Double)
+      NumA              -> singleton $ NumA
+      StrA              -> singleton $ NumA
+      BoolA             -> fromList $ [ pinject (0::Double) , pinject (1::Double) ]
+      (CloA _)          -> undefined -- TODO: Does lambdajs need these?
+      (ObjA _)          -> undefined -- TODO: Does lambdajs need these?
+      (LocA _)          -> undefined -- This isn't part of real JS, should it be here?
+    primToString v = case v of
+      (LitA NullL     ) -> singleton $ pinject "null"
+      (LitA UndefinedL) -> singleton $ pinject "undefined"
+      (LitA (B b)     ) -> singleton $ pinject $ if b then "true" else "false"
+      (LitA (N n)     ) -> singleton $ pinject $ show n -- see 9.8.1, this is most certainly wrong, but it's easy (trollface)
+      (LitA (S s)     ) -> singleton $ pinject s
+      NumA              -> singleton $ StrA
+      StrA              -> singleton $ StrA
+      BoolA             -> fromList $ [ pinject "true" , pinject "false" ]
+      (CloA _)          -> undefined -- TODO: Does lambdajs need these?
+      (ObjA _)          -> undefined -- TODO: Does lambdajs need these?
+      (LocA _)          -> undefined -- This isn't part of real JS, should it be here?
+    primToBool v = case v of
+      (LitA NullL     ) -> singleton $ pinject False
+      (LitA UndefinedL) -> singleton $ pinject False
+      (LitA (B b)     ) -> singleton $ pinject b
+      (LitA (N n)     ) -> singleton $ pinject $ if (Prelude.isNaN n || n == 0) then False else True
+      (LitA (S s)     ) -> singleton $ pinject $ if (null s) then False else True
+      NumA              -> singleton $ BoolA
+      StrA              -> singleton $ BoolA
+      BoolA             -> singleton $ BoolA
+      (CloA _)          -> singleton $ pinject True
+      (ObjA _)          -> singleton $ pinject True
+      (LocA _)          -> undefined -- TOOD: This isn't part of real JS, should it be here?
+    isPrim v = case v of
+      (LitA _) -> singleton $ pinject True
+      NumA     -> singleton $ pinject True
+      StrA     -> singleton $ pinject True
+      BoolA    -> singleton $ pinject True
+      (CloA _) -> singleton $ pinject False
+      (ObjA _) -> singleton $ pinject False
+      (LocA _) -> undefined -- TODO: This isn't part of real JS, should it be here?
+    hasOwnProp o f = case o of
+      (ObjA (Obj kvs)) -> case f of
+        (LitA (S name)) -> singleton $ pinject $ maybeElim False (const True) $ kvs # name
+        StrA            -> fromList $ [ pinject True , pinject False ]
+        _               -> undefined -- TODO: Does this ever happen?
+      _ -> undefined -- TODO: does this ever happen?
